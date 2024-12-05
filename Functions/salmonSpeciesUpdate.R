381564e028994d3bfeba2ea58e82482af9fe712c
Liz Allyn <liz.allyn@allyn.org>
2024-12-05 14:43:24 -0800

    created a generic version

--- Functions/salmonSpeciesUpdate.R
+++ Functions/salmonSpeciesUpdate.R
@@ -1,21 +1,20 @@
-# function that returns the arriving salmon of each species for a given day of the year
-# !!!!If you add steelhead data make sure to fix the data stream reference below!!!!
+# function that adds the arriving salmon of each run
+# for a given day of the year
 
-salmonSpeciesUpdate <- function(day, sockeye = 0, chinook = 0, coho = 0, chum = 0, pink = 0, steelhead = 0, data) {
-  Chinook_day <- chinook + (data %>% slice(day) %>% pull(Chinook))
-  Sockeye_day <- sockeye + (data %>% slice(day) %>% pull(Sockeye))
-  Coho_day <- coho + (data %>% slice(day) %>% pull(Coho)) 
-  # Chum_day <- chum + (data %>% slice(day) %>% pull(Chum))
-  # Pink_day <- pink + (data %>% slice(day) %>% pull(Pink)) 
-  # Steelhead_day <- steelhead + (data %>% slice(day) %>% pull(Steelhead))
-  #
- return(data.frame(Chinook = Chinook_day,
-             Sockeye = Sockeye_day,
-             Coho = Coho_day,
-             # Chum = Chum_day,
-             # Pink = Pink_day,
-             # Steelhead = Steelhead_day, 
-             row.names = c("count"))
- )
+library(tidyr)
+library(dplyr)
+
+
+
+salmonSpeciesUpdate <- function(day, salmon_list, arrive_data) {
+  onthisday <- NA
+  for (i in 1:nrow(salmon_list)){
+    onthisday[i] <- salmon_list[i, "Count"] + 
+      (arrive_data %>% slice(day) %>% pull(salmon_list$Run[i]))
+  }
+  return(data.frame(Run = salmon_list$Run,
+                    Count = onthisday))
 }
-# salmonSpeciesUpdate(12, sockeye = 1, data = Arrive_fish)
+
+# salmon_list <- data.frame(Run = run_info$Run, Count = c(150, 10))
+# salmonSpeciesUpdate(20, salmon_list, arrive_data = salmon_arrival)
