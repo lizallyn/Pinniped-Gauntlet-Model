a0a731c19075e87bf36d578ef325b5674df044a3
Liz Allyn <liz.allyn@allyn.org>
2024-12-05 17:59:21 -0800

    cleanup

--- RunTheModel/Prep_Salmon_Data.R
+++ RunTheModel/Prep_Salmon_Data.R
@@ -18,6 +18,8 @@ n_species <- nrow(run_info)
 salmon_arrival <- create_salmon_arrival(6, run_info, arrival)
 colnames(salmon_arrival) <- c("Day", run_info$Run)
 n_days <- nrow(salmon_arrival)
+data_start <- salmon_arrival$Day[1]
+data_end <- max(salmon_arrival$Day)
 
 # test it out:
 # days <- test$Day[1]:max(test$Day)
@@ -32,6 +34,9 @@ Run2_fish_rate <- 0.025
 
 Run1_fishery_days <- 70:80
 Run2_fishery_days <- NA
+# fishery_range <- fishery_open:fishery_close
+# # convert to loop t from dayofyear
+# fishery_days <- fishery_range[which(fishery_range %in% day_range)] - (start_loop - 1)
 
 Run1_catch_rate <- rep(0, nrow(salmon_arrival))
 Run1_catch_rate[Run1_fishery_days] <- Run1_fish_rate
@@ -40,3 +45,6 @@ Run2_catch_rate[Run2_fishery_days] <- Run2_fish_rate
 
 salmon_catch_rates <- data.frame(salmon_arrival$Day, Run1_catch_rate, Run2_catch_rate)
 colnames(salmon_catch_rates) <- colnames(salmon_arrival)
+
+harvest_open <- 70
+harvest_close <- 80
