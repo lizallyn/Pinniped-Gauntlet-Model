9ca014528a2b05b0d12d5eea75753aeede0eb8da
Liz Allyn <liz.allyn@allyn.org>
2024-12-16 14:25:57 -0800

    ok a manipulations version exists?

--- ParameterManipulations/specialist_prob.R
+++ ParameterManipulations/specialist_prob.R
@@ -28,23 +28,6 @@ for(i in 1:length(specialist_prob)){
   
   # 07 Run The Loop
   source("ParameterManipulations/runLoop.R")
-  if(no_seals == F && no_zc == F && no_ej == F){
-    source("Functions/rungeKutta_3.R")
-    source("RunTheModel/06_The_Loop.R")
-    source("Functions/Plots_Ej.R")
-    source("Functions/Plots_Zc.R")
-  } else if(no_seals == F && no_zc == F && no_ej == T){
-    source("Functions/rungeKutta_2.R")
-    source("RunTheModel/06_The_Loop_pv_zc.R")
-    source("Functions/Plots_Zc.R")
-  } else if(no_seals == F && no_zc == T && no_ej == F){
-    source("Functions/rungeKutta_2.R")
-    # add if needed later
-    source("Functions/Plots_Ej.R")
-  } else if(no_seals == F && no_zc == T && no_ej == T){
-    source("Functions/rungeKutta.R")
-    source("RunTheModel/06_The_Loop_pv.R")
-  } else {print("Error in pinniped accounting! They cannot count and neither can you!")}
   
   # 08 Plots
   source("Functions/Plots_Pv.R")
